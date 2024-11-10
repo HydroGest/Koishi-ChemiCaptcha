be30a7e0dc03e772317abe92cb4c1b944606267e
白羽 <1665169869@qq.com>
2024-11-10 22:42:37 +0800

    优化验证逻辑，增加配置项和提示信息

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { Context, Random, Schema, Session } from "koishi";
+import { Context, h, Random, Schema, Session } from "koishi";
 import {} from "koishi-plugin-adapter-onebot";
 import {} from "koishi-plugin-cache-database";
 
@@ -9,9 +9,62 @@ export const inject = {
 };
 
 export interface Config {
-  maxAge: number;
+  vCodeExp: number;
   // 白名单群号配置
   attempts: number;
+  groupNewMemberVerificationMessage: string;
+  groupChatVerificationTimeoutMsg: string;
+  humanVerificationSuccessMessage: string;
+  humanVerificationFailedKickMessage: string;
+  humanVerificationAnswerFailedMessage: string;
+}
+
+export const Config: Schema<Config> = Schema.object({
+  vCodeExp: Schema.number()
+    .default(1000 * 60 * 3)
+    .description("验证码有效时间(毫秒)"),
+  attempts: Schema.number()
+    .default(3)
+    .description("最大尝试次数")
+    .min(1)
+    .max(9),
+  groupNewMemberVerificationMessage: Schema.string()
+    .role("textarea", { rows: [2, 10] })
+    .description(
+      "新人入群提醒信息 使用{ @at }艾特新人<br> 可选变量: calcVal1: 随机数1, calcVal2: 随机数2, vCodeExp: 验证码有效期, attempts: 剩余尝试次数"
+    )
+    .default(
+      "欢迎{ @at } 加入本群！请完成群验证。\n{ calcVal1 } + { calcVal2 } = ?\n有效期为：{ vCodeExp } 秒，请在有效期内完成验证\n (tips: 你只有{ attempts }次机会，超过{ attempts }次将被移出本群)"
+    ),
+  groupChatVerificationTimeoutMsg: Schema.string()
+    .role("textarea", { rows: [2, 10] })
+    .default("{ @at } 验证超时，你已被移出本群")
+    .description("验证超时信息 使用{ @at }艾特新人"),
+  humanVerificationSuccessMessage: Schema.string()
+    .role("textarea", { rows: [2, 10] })
+    .default(
+      "{ @at } 验证成功，欢迎加入本群！\n 请遵守群规，文明发言\n 进群可先查看群公告"
+    )
+    .description("验证成功信息 使用{ @at }艾特新人"),
+  humanVerificationFailedKickMessage: Schema.string()
+    .role("textarea", {
+      rows: [2, 10],
+    })
+    .default("{ @at } 验证失败，失败次数达到上限，你已被移出本群")
+    .description("验证失败信息 使用{ @at }艾特新人"),
+  humanVerificationAnswerFailedMessage: Schema.string()
+    .role("textarea", { rows: [2, 10] })
+    .default("{ @at } 验证失败，你还有{ attempts }次机会")
+    .description(
+      "验证码错误信息 使用{ @at }艾特新人<br> 可选变量: attempts: 剩余尝试次数"
+    ),
+});
+
+interface CaptchaCache {
+  attempts: number;
+  result: number;
+  createdAt: number;
+  timerId: number;
 }
 
 function isUndefined(value: any): boolean {
@@ -40,69 +93,103 @@ function clearTimer(timerId: number) {
   }
 }
 
-export const Config: Schema<Config> = Schema.object({
-  maxAge: Schema.number()
-    .default(1000 * 60 * 3)
-    .description("验证码有效时间(毫秒)"),
-  attempts: Schema.number()
-    .default(3)
-    .description("最大尝试次数")
-    .min(1)
-    .max(9),
-});
+/**
+ * 解析配置提示信息文本，转为JSX
+ * @param msg 配置提示信息文本
+ */
+function parseMsgToJSX(
+  msg: string,
+  args: { userId?: string; [x: string]: any }
+): h[] {
+  msg = msg.replaceAll(/\{\s*(\@at)\s*\}+/g, `<at id="${args["userId"]}"/>`);
 
-interface CaptchaCache {
-  attempts: number;
-  result: number;
-  createdAt: number;
-  timerId: number;
+  const valMatchs = [...msg.matchAll(/\{\s*(\w+)\s*\}+/g)];
+  for (const match of valMatchs) {
+    // 如果配置参数不存在，则跳过 可有效避免报错或返回undefined的情况
+    if (isUndefined(args[match[1]])) {
+      continue;
+    }
+    msg = msg.replace(match[0], args[match[1]]);
+  }
+
+  return h.parse(msg);
 }
 
 export function apply(ctx: Context, config: Config) {
   // write your plugin here
 
-  // TODO 未完成逻辑：超时后踢出群员（难点：无法保存计时器Id/销毁计时器的函数到cache）
+  ctx.on("ready", () => {
+    ctx.logger.info("captcha plugin loaded");
+  });
 
   ctx.on("guild-member-added", async (session: Session) => {
     const { userId, guildId, type } = session;
-    const { maxAge, attempts } = config;
+    const { vCodeExp, attempts } = config;
 
-    const a = Random.int(1, 100);
-    const b = Random.int(1, 100);
+    const calcVal1 = Random.int(1, 100);
+    const calcVal2 = Random.int(1, 100);
 
     ctx.logger.info(guildId, userId, type);
 
     try {
+      const args = {
+        userId,
+        calcVal1,
+        calcVal2,
+        attempts,
+        vCodeExp: (vCodeExp / 1000).toFixed(0),
+      };
+
       await session.send(
-        <>
-          欢迎 <at id={userId} /> 加入本群！请完成群验证。
-          <br />
-          {a} + {b} = ?
-          <br />
-          有效期为：{(maxAge / 1000).toFixed(2)}秒，请在有效期内完成验证
-          <br />
-          (tips: 你只有{attempts}次机会，超过{attempts}次将被移出本群)
-        </>
+        parseMsgToJSX(config.groupNewMemberVerificationMessage, args)
       );
 
       const value: CaptchaCache = {
         attempts: 0,
-        result: a + b,
+        result: calcVal1 + calcVal2,
         createdAt: Date.now(),
         timerId: createTimer(async () => {
+          const captchaCache: CaptchaCache | undefined = await ctx.cache.get(
+            "default",
+            `captcha:${userId}`
+          );
+
+          if (isUndefined(captchaCache)) {
+            ctx.logger.info("验证码缓存不存在，跳过定时器" + userId);
+            return;
+          }
+
           ctx.logger.info("验证码超时，移出群 " + userId);
           await session.send(
-            <>
-              <at id={userId} /> 验证超时，你已被移出本群
-            </>
+            parseMsgToJSX(config.groupChatVerificationTimeoutMsg, { userId })
           );
           session.onebot.setGroupKick(guildId, userId, false);
-        }, maxAge),
+        }, vCodeExp),
       };
-      await ctx.cache.set("default", `captcha:${userId}`, value, maxAge);
+      await ctx.cache.set("default", `captcha:${userId}`, value, vCodeExp);
     } catch (error) {
       ctx.logger.error(error);
       session.send("入群验证码插件出错, 请联系管理员");
+    }
+  });
+
+  ctx.on("guild-member-removed", async (session: Session) => {
+    // 如果待验证群员被移除群或退出群，则删除定时器缓存
+    // 识别成员退出需管理员权限
+    const { userId } = session;
+
+    let captchaCache: CaptchaCache | undefined;
+    captchaCache = await ctx.cache.get("default", `captcha:${userId}`);
+    if (isUndefined(userId) || isUndefined(captchaCache)) {
+      return;
+    } else {
+      ctx.logger.info(
+        "captcha plugin: 识别到待验证群员被移除群或退出群，清除定时器 && 删除缓存"
+      );
+      return Promise.all([
+        clearTimer(captchaCache.timerId),
+        ctx.cache.delete("default", `captcha:${userId}`),
+      ]);
     }
   });
 
@@ -130,12 +217,9 @@ export function apply(ctx: Context, config: Config) {
     if (captchaCache.result === Number(content)) {
       clearTimer(captchaCache.timerId);
       await session.send(
-        <>
-          <at id={userId}></at>验证成功，欢迎你的到来~
-          <br />
-          新人先看群公告
-        </>
+        parseMsgToJSX(config.humanVerificationSuccessMessage, { userId })
       );
+
       await ctx.cache.delete("default", `captcha:${userId}`);
     } else {
       // 撤回未验证群员的消息
@@ -146,23 +230,20 @@ export function apply(ctx: Context, config: Config) {
         // 验证超过上限，移除本群
         clearTimer(captchaCache.timerId);
         await session.send(
-          <>
-            <at id={userId} />
-            验证失败，你已被移出本群。
-          </>
+          parseMsgToJSX(config.humanVerificationFailedKickMessage, { userId })
         );
 
         session.onebot?.setGroupKick(guildId, userId, false);
       } else {
-        const maxAge: number = Date.now() - captchaCache.createdAt;
+        const vCodeExp: number = Date.now() - captchaCache.createdAt;
 
         Promise.all([
-          ctx.cache.set("default", `captcha:${userId}`, captchaCache, maxAge),
+          ctx.cache.set("default", `captcha:${userId}`, captchaCache, vCodeExp),
           session.send(
-            <>
-              <at id={userId} /> 答案错误,你还有
-              {attempts - captchaCache.attempts}次机会
-            </>
+            parseMsgToJSX(config.humanVerificationAnswerFailedMessage, {
+              attempts: attempts - captchaCache.attempts,
+              userId,
+            })
           ),
           session.onebot.deleteMsg(messageId),
         ]);
